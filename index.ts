96bd73a07de510fc5515a9e0e0d93ccb304febe3
PRAJYOT <prajyotnarulkar@gmail.com>
2025-02-20 11:44:35 +0530

    pulumi deploy an s3 bucket

---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.Bucket("my-demo-bucket", {
+    acl: "private",
+    tags: {
+        Name: "my-demo-bucket",
+    },
+    lifecycleRules: [{
+        enabled: true,
+        expiration: {
+            days: 365,
+        },
+    }],
+});
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("my-bucket-public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: true,
+    ignorePublicAcls: true,
+    blockPublicPolicy: true,
+    restrictPublicBuckets: true,
+});
+
+export const bucketName = bucket.bucket;
+export const bucketArn = bucket.arn;
